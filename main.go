f485744caf85b7fb1c935fbdb72f74b555f081e4
jkarlos000 <jc.ulloa@jkarlos.info>
2020-04-09 00:15:24 -0400

    Get product by ID

--- main.go
+++ main.go
@@ -1,33 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"github.com/go-chi/chi"
 	_ "github.com/go-sql-driver/mysql"
+	"net/http"
 	"restapi/database"
+	"restapi/product"
 )
 
 func main() {
-	/*r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
-	http.ListenAndServe(":3000", r)*/
 	databaseConnection := database.InitDB()
-
-	// Logic
 	defer databaseConnection.Close()
-	fmt.Println(databaseConnection)
+	// Logic
+	var productRepository = product.NewRepository(databaseConnection)
+	var productService product.Service
+	productService = product.NewService(productRepository)
 
-	pachonsitos := [...]string{
-		"Lirlis", "bb",
-	}
-	fmt.Printf("%T\n", pachonsitos)
-	printString(pachonsitos[:]...)
-}
-
-func printString(x ...string) {
-	for _, v := range x {
-		fmt.Println(v)
-	}
+	r := chi.NewRouter()
+	r.Mount("/products", product.MakeHttpHandler(productService))
+	http.ListenAndServe(":3000", r)
 }
